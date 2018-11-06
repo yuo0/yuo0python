20090d85339c2d5228489c75c9cad9a3c83a5941
yuo0 <1254712628@qq.com>
2018-11-06 10:35:01 +0800

    yi

---
+++ py/lianxi.py
@@ -0,0 +1,18 @@
+#标识符不能为数字、特殊符号开头，_可以,特殊字符不可以任意长度，关键字不能作为标识符
+#方法也叫函数，定义方法：def 方法名（）：  ，调用方法：方法名（）。
+a1=input("qingshuru:")
+print(a1)
+if a1>"5":
+    print("a1dayu5")
+def ad():
+    a2 = input("shuru")
+    if int(a2) > 5:
+        print("adayu5")
+    else:
+        print("xiaoyu")
+ad()
+
+a4="123"
+print(type(a4))
+
+
